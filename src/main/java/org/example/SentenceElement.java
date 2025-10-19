c03d1d3542080abd3d0559e4dcd80de402ecf9b5
dk872 <dimakulyk2005@gmail.com>
2025-10-19 12:43:26 +0300

    Add SentenceElement interface

---
+++ src/main/java/org/example/SentenceElement.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/**
+ * A marker interface for components that can be part of a sentence.
+ * This provides a common type for classes like {@link Word} and {@link Punctuation}.
+ */
+public interface SentenceElement {
+    // This is a marker interface, so it has no methods.
+}
